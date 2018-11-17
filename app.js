23d8ebb719c7f43648564b668d4c7035fff4f8fd
Collins Juma <collinsjumaz42@gmail.com>
2018-11-17 10:58:08 +0300

    Serving Index.html

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var express = require ('express');
+var express = require('express');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
-app.get('/' function(req, res){
-	)}
+app.use(morgan('tiny'));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, '/views/index.html'));
+	})
+app.listen(3000, function(){
+	debug('listening on port 3000');
+
+	});
